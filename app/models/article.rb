e7b48d7c6a1de118e53ae1bf84b53ba303200655
george <georgij-net@yandex.ru>
2018-03-03 00:40:54 +0300

    add state field to Articles with possible values draft and published

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
   validates :author_name, presence: true
+  validates :state, inclusion: { in: %w[draft published] }
 end
