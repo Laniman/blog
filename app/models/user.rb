321b2c6a9523d1ef237624f786814a4a015e3406
george <georgij-net@yandex.ru>
2018-03-03 09:11:56 +0300

    add User validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_email_format_of :email, message: 'is not looking good'
 end
